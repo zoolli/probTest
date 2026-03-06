d19d7d991d3a7137b035ec5a4e7e63a4baf52e31
zool <zool@championgame.net>
2026-03-06 14:38:50 +0800

    1st

--- projects/0002-math/src/prob/extends/IGameSpinResult.ts
+++
@@ -1,9 +0,0 @@
-import { IGameSelectedInfo } from "./IGameSelectedInfo";
-
-export interface IGameSpinResult {
-  gameSelectingInfo?: IGameSelectedInfo;
-
-  ngReels?: Array<Array<number>>;
-
-  resultHistory: number[];
-}
