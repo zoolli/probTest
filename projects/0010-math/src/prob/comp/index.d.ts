8c9bab382e8d53104b2b5585b4e3a57161174489
zool <zool@championgame.net>
2026-03-09 10:49:54 +0800

    update 0000 as 0010

--- projects/0010-math/src/prob/comp/index.d.ts
+++
@@ -1,6 +0,0 @@
-// export import GameState = require("./game_state");
-
-export { GameState } from "./game_state";
-// export { MachineData } from "./machine_data";
-// export { UserData } from "./user_data";
-export { SwitchChance } from "./switch_chance";
